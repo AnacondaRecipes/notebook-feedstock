653105f7038a6d1e96ea2f937e5bec24f03bda6d
Marco Esters <mesters@anaconda.com>
2024-08-30 15:30:25 -0700

    Fix path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,11 +2,16 @@ REM Prepare shortcuts. menuinst v2 shortcuts should only be used startings
 REM at menuinst v2.1.1 due to bugs. The post-link script
 REM will handle which shortcut to use. One file needs to be the default
 REM menu file so that conda picks it up when running menuinst.
-IF NOT EXIST %PREFIX%\Menu mkdir %PREFIX%\Menu
-copy %RECIPE_DIR%\menu-v1.json %PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak
-copy %RECIPE_DIR%\menu-v2.json %PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak
-copy %RECIPE_DIR%\menu-v2.json %PREFIX%\Menu\%PKG_NAME%_menu.json
-copy %RECIPE_DIR%\jupyter.ico %PREFIX%\Menu\
+SET MENU_DIR=%PREFIX%\Menu
+IF NOT EXIST %MENU_DIR% mkdir %MENU_DIR%
+if errorlevel 1 exit 1
+copy %RECIPE_DIR%\menu-v1.json %MENU_DIR%\%PKG_NAME%_menu-v1.json.bak
+if errorlevel 1 exit 1
+copy %RECIPE_DIR%\menu-v2.json %MENU_DIR%\%PKG_NAME%_menu-v2.json.bak
+if errorlevel 1 exit 1
+copy %RECIPE_DIR%\menu-v2.json %MENU_DIR%\%PKG_NAME%_menu.json
+if errorlevel 1 exit 1
+copy %RECIPE_DIR%\jupyter.ico %MENU_DIR%\jupyter.ico
 if errorlevel 1 exit 1
 
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
