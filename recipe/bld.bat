59cc93d6bb077fc31e6536fd0a56a48a4019815c
Marco Esters <mesters@anaconda.com>
2024-08-30 15:19:54 -0700

    Convert to menuinst v2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,11 @@
-set MENU_DIR=%PREFIX%\Menu
-mkdir %MENU_DIR%
-
+REM Prepare shortcuts. menuinst v2 shortcuts should only be used startings
+REM at menuinst v2.1.1 due to bugs. The post-link script
+REM will handle which shortcut to use. One file needs to be the default
+REM menu file so that conda picks it up when running menuinst.
+IF NOT EXIST %PREFIX%\Menu mkdir %PREFIX%\Menu
+copy %RECIPE_DIR%\menu-v1.json %PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak
+copy %RECIPE_DIR%\menu-v2.json %PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak
+copy %RECIPE_DIR%\menu-v2.json %PREFIX%\Menu\%PKG_NAME%_menu.json
 copy %RECIPE_DIR%\jupyter.ico %MENU_DIR%
 if errorlevel 1 exit 1
 
